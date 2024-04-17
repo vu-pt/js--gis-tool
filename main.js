d4dabbfc235a9d98719167f730b21712538ae894
vu-pt <75203465+vu-pt@users.noreply.github.com>
2024-04-17 18:28:46 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,99 @@
+var map;
+var list_points = [];
+var string_points = "";
+var toDeg = function (rad) { return rad * 180 / Math.PI; }
+var toRad = function (deg) { return deg * Math.PI / 180; }
+function copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+        fallbackCopyTextToClipboard(text);
+        return;
+    }
+    navigator.clipboard.writeText(text).then(function () {
+        console.log('Async: Copying to clipboard was successful!');
+    }, function (err) {
+        console.error('Async: Could not copy text: ', err);
+    });
+}
+require([
+    "esri/Map",
+    "esri/views/MapView",
+    "esri/layers/GraphicsLayer",
+    "esri/Graphic"
+], function (Map, MapView, GraphicsLayer, Graphic) {
+    map = new Map({
+        basemap: "topo-vector"
+    });
+    var view = new MapView({
+        container: "viewDiv",
+        map: map,
+        center: [106.80196068432218, 10.870525057480787],
+        zoom: 16
+    });
+    var graphicsLayer = new GraphicsLayer();
+    var drawPoint = function (x, y, second) {
+        const color = second ? [255, 255, 0] : [255, 255, 255];
+        return new Graphic({
+            geometry: {
+                type: "point",
+                longitude: x,
+                latitude: y
+            },
+            symbol: {
+                type: "simple-marker",
+                color: [0, 153, 51],
+                outline: {
+                    color: color,
+                    width: 1
+                }
+            }
+        });
+    };
+
+    function createNewPoint(coord, bearing, distance) {
+        /** http://www.movable-type.co.uk/scripts/latlong.html
+         φ is latitude, λ is longitude, 
+         θ is the bearing (clockwise from north), 
+         δ is the angular distance d/R; 
+         d being the distance travelled, R the earth’s radius*
+         **/
+        var
+            radius = 6371e3, //meters
+            δ = Number(distance) / radius, // angular distance in radians
+            θ = toRad(Number(bearing)),
+            φ1 = toRad(Number(coord[1])),
+            λ1 = toRad(Number(coord[0]));
+
+        var φ2 = Math.asin(Math.sin(φ1) * Math.cos(δ) + Math.cos(φ1) * Math.sin(δ) * Math.cos(θ));
+
+        var λ2 = λ1 + Math.atan2(Math.sin(θ) * Math.sin(δ) * Math.cos(φ1), Math.cos(δ) - Math.sin(φ1) * Math.sin(φ2));
+
+        λ2 = (λ2 + 3 * Math.PI) % (2 * Math.PI) - Math.PI; // normalise to -180..+180°
+
+        return [toDeg(λ2), toDeg(φ2)]; //[lon, lat]
+    }
+
+    view.popup.autoOpenEnabled = false; // Disable the default popup behavior
+    view.on("click", function (event) { // Listen for the click event
+        view.hitTest(event).then(function (hitTestResults) { // Search for features where the user clicked
+            if (hitTestResults.results) {
+                if ($('#z1').val()) {
+                    list_points.push([event.mapPoint.longitude, event.mapPoint.latitude, Number($('#z1').val())]);
+                } else {
+                    list_points.push([event.mapPoint.longitude, event.mapPoint.latitude]);
+                }
+                $('#x1').val(event.mapPoint.longitude);
+                $('#y1').val(event.mapPoint.latitude);
+                graphicsLayer.removeAll();
+                graphicsLayer.add(drawPoint(event.mapPoint.longitude, event.mapPoint.latitude));
+                if ($('#d').val() && $('#a').val()) {
+                    const newPoint = createNewPoint([$('#x1').val(), $('#y1').val()], $('#a').val(), $('#d').val());
+                    $('#x2').val(newPoint[0]);
+                    $('#y2').val(newPoint[1]);
+                    graphicsLayer.add(drawPoint(newPoint[0], newPoint[1]));
+                }
+                $('#points').val(JSON.stringify(list_points));
+            }
+        })
+    });
+    map.add(graphicsLayer);
+});
